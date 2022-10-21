b0b2633e651f5a81d3b06538e090291424d02889
Beth Njuguna <libbybetha@gmail.com>
2022-10-21 14:31:31 +0300

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,34 @@
+class Book 
+    attr_reader  :title
+    attr_reader  :author
+    attr_reader  :page_count
+    attr_reader   :genre
+    def initialize (title)
+        @title = title
+        
+    end 
+    def author=(author)
+        @author = author
+    end
 
+def page_count=(page_count)
+    @page_count = page_count
+end
 
+def genre=(genre)
+    @genre = genre
+end
+
+def turn_page
+    puts "Flipping the page...wow, you read fast!"
+end
+
+end
+ book = Book.new("And Then There Were None")
+book.title 
+author= Book.new("Agatha Christie")
+author.author
+page_count = Book.new(272)
+page_count.page_count
+genre = Book.new("Mystery")
+genre.genre
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,27 @@
 # Make your shoe class here!
+class Shoe
+    attr_reader  :brand
+    attr_reader   :color
+    attr_reader   :size
+    attr_reader   :material
+    attr_reader    :condition
+def initialize (brand)
+    @brand = brand 
+end
+def color=(color)
+    @color = color
+end
+def size=(size)
+    @size = size
+end
+def material=(material)
+    @material = material
+end
+def condition=(condition)
+    @condition = condition
+end
+def cobble 
+  @condition = "new"
+  puts  "Your shoe is as good as new!"
+end 
+end
